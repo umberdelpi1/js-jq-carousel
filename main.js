ab1093a1dbcf4160b938a2b9cc37db4c27b798b0
Umberto Del Piano <umberdelpi1@gmail.com>
2020-04-02 18:42:41 +0200

    HTML & CSS terminata

--- main.js
+++ main.js
@@ -1,1 +1,63 @@
-console.log('ciao');
+// regola generale del documento per assicurami ulteriore check
+
+// *************************** CREAZIONE DI UNA SLIDE *********************
+$(document).ready(function(){
+  // effettuo click sulla classe '.icon-prev' inserendo 'virgolette'
+  $('.icon-prev').click(
+    // inserisco funzione di Callback
+
+  );
+// effettuo click sulla classe '.icon-next' inserendo 'virgolette'
+  $('.icon-next').click(
+    // inserisco funzione di Callback
+    nextImage
+  );
+
+// funzione nextImage
+function nextImage(){
+  // creo variabile con riferimento alla classe .image img.active (1 img della slide)
+  var imgActive = $('.image img.active ');
+  // controllo in console quello che succede
+  console.log('nextImage');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
